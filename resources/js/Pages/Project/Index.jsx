46693c330ccb5471468ccd883c6403afc2682b56
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2024-04-05 15:37:51 -0400

    1:38 filtering projects

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,6 @@
 import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import { Head, Link } from '@inertiajs/react'; 
 
 export default function Index({auth, projects}){
@@ -51,7 +52,7 @@ export default function Index({auth, projects}){
                                 </thead>
                                 <tbody>
                                     {projects.data.map(project => (
-                                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
                                        <td className="px-3 py-2">
                                         {project.id}
                                        </td>
@@ -62,7 +63,13 @@ export default function Index({auth, projects}){
                                        {project.name}
                                        </td>
                                        <td className="px-3 py-2">
-                                       {project.status}
+                                        <span className={"px-2 py-1 rounded " +
+                                            PROJECT_STATUS_CLASS_MAP[project.status]
+                                         }>
+                                            <span className="text-white">
+                                        {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                        </span>
+                                        </span>
                                        </td>
                                        <td className="px-3 py-2 text-nowrap">
                                        {project.created_at}
